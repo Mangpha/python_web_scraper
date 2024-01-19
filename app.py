81fba4f1022d18fc63a8ffc443eb57ce6614d336
Mangpha <8350130@gmail.com>
2024-01-19 20:23:11 +0900

    Render Template

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home() -> str:
-    return "Hello"
+def home():
+    return render_template("index.html", name="Mangpha")
+
+
+@app.route("/hello")
+def hello():
+    return "Hi There"
 
 
 if __name__ == "__main__":
