5592c79125da1e4807b231ac33a7f86709fe65ff
Mangpha <8350130@gmail.com>
2024-01-20 09:42:20 +0900

    Cache

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from file import SearchAndCreateData
 
 app = Flask(__name__)
+
+DB = {}
 
 
 @app.route("/")
@@ -10,7 +13,21 @@ def home():
 
 @app.route("/search")
 def search():
-    return render_template("search.html")
+    keyword = request.args.get("keyword")
+    # keywords = [
+    #     "flutter",
+    #     "python",
+    #     "kotlin",
+    # ]
+
+    # for i in range(len(keywords)):
+    #     data = SearchAndCreateData(keywords[i])
+    #     DB[keywords[i]] = data.Search()
+    #     data.CreateCSV(DB[keywords[i]])
+    if keyword not in DB:
+        data = SearchAndCreateData(keyword)
+        DB[keyword] = data.Search()
+    return render_template("search.html", keyword=keyword, jobs=DB[keyword])
 
 
 if __name__ == "__main__":
